a6375658fb5eb712da23dc161297262cfbda5520
enemarina <eml_ene@yahoo.com>
2017-12-03 22:03:41 +0200

    minor UI improovements

--- BackLogApp/BackLogApp/Controllers/HomeController.cs
+++ BackLogApp/BackLogApp/Controllers/HomeController.cs
@@ -8,11 +8,7 @@ namespace BackLogApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
+    
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
